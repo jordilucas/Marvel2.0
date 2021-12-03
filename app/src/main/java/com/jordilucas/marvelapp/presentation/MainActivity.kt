92e25987f18cfa4dac60004db992d9f44ec679bf
Jordi <jordilucas3@gmail.com>
2021-12-03 08:08:05 -0300

    Configuracao modulos do app

--- app/src/main/java/com/jordilucas/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/jordilucas/marvelapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jordilucas.marvelapp
+package com.jordilucas.marvelapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jordilucas.marvelapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
