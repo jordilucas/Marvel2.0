8bc7dc0776b4fa2d33e5b9a813fe4c1710d09407
Jordi Lucas <jordi.santos@lumetec.com.br>
2021-12-31 10:33:21 -0300

    Configuração application

---
+++ app/src/main/java/com/jordilucas/marvelapp/MarvelApp.kt
@@ -0,0 +1,10 @@
+package com.jordilucas.marvelapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Jordi Lucas on 17/12/2021.
+ */
+@HiltAndroidApp
+class MarvelApp : Application()
--- app/src/main/java/com/jordilucas/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/jordilucas/marvelapp/presentation/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.jordilucas.marvelapp.R
 import com.jordilucas.marvelapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
