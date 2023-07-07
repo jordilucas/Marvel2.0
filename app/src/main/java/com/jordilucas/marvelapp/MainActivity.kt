256568437c1042e590973be98cba29be48fe299a
Jordi <jordilucas3@gmail.com>
2023-07-07 08:35:44 -0300

    feature(atualizacao) Atualizacao de libs, inicio de migraçao para jetpack compose, novos styles e temas

---
+++ app/src/main/java/com/jordilucas/marvelapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.jordilucas.marvelapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.jordilucas.marvelapp.ui.theme.MarvelApp20Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*setContent {
+            MarvelApp20Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }*/
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+        color =  Color.Black
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MarvelApp20Theme {
+        Greeting("Android")
+    }
+}
